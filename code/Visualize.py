b4f7ed085e6fe9cfcfe3326ddc5e789d8244723e
Luca Hackl <63552660+Luca-Hackl@users.noreply.github.com>
2021-01-27 12:08:41 +0100

    Added scatter plot

--- code/Visualize.py
+++ code/Visualize.py
@@ -15,11 +15,23 @@ def stats(county):
 
     parameter = county[0:1]
     if parameter == "/":
-        if county.find(" vs ") != -1:
+        if county.find(" vs ") != -1 and county[-7:] != "scatter":
             
             img = statscompare(county)
+
+        elif county[-7:] == "scatter":
+            if county.find(" vs ") != -1:
+
+                img = scatterplotcomp(county)
+                
+            
+            else:
+            
+                img = scatterplot(county)
+
+
         else:
-            img = piechart(county[1:])
+            img = piechart(county)
         
         return img
     
@@ -83,6 +95,125 @@ def barplot(county):
         
         imgdata = 'saved_figure.png'    #save and return the img path file
         return imgdata
+
+def scatterplot(county):
+    
+    county = county [1:-7]
+
+    try: 
+        os.remove('saved_figure.png')   #remove old img data
+    except: 
+        print("Datei nicht auffindbar")
+    finally:
+
+        mydb = statistics.SQLconnect() #connects to SQL server
+        mycursor = mydb.cursor()
+
+        sql_select_query  = """SELECT * FROM landkreis WHERE Stadtname = %s ORDER BY Zuletzt_geupdatet DESC"""  #SQL query
+        mycursor.execute(sql_select_query,(county,))    #takes input from DiscordBotDebug and puts in in %s above
+
+        myresult = mycursor.fetchall()
+
+        mydb.close()
+        date = []
+        incidence = []
+
+        for x in myresult:      #search trough results of query
+           
+            date.append(str(x[6]))      
+            incidence.append(float(x[5]))
+        
+        df = pd.DataFrame({county: incidence},
+                             index=date) 
+        
+        df = df[::-1]
+        ax = df.plot()
+
+        plt.xticks(rotation=30, horizontalalignment="center")
+        plt.title('Inzidenzwert von ' + county)  #name axis and title
+        plt.ylabel('Inzidenzfälle')
+        plt.xlabel('Datum')   
+
+        ax.set_ylim(bottom=0)
+
+        figName = 'saved_figure.png'
+        fig = plt.gcf()
+        fig.set_size_inches((8.5, 11), forward=False)
+        fig.savefig(figName, dpi=500)
+        
+        imgdata = 'saved_figure.png'    #save and return the img path file
+        return imgdata
+
+
+def scatterplotcomp(county):
+    
+    county = county [:-7]
+
+    try: 
+        os.remove('saved_figure.png')   #remove old img data
+    except: 
+        print("Datei nicht auffindbar")
+    finally:
+
+        comparecounties = []
+
+        vsindex = county.index(" vs ")
+        secondcounty = vsindex + 4 #skips the " vs " trough indexing
+
+        comparecounties.append(county[1:vsindex])   #takes county bevore the "vs"
+        comparecounties.append(county[secondcounty:-1]) #takes the second county 
+
+       
+        mydb = statistics.SQLconnect() #connects to SQL server
+        mycursor = mydb.cursor()
+        
+        incidence = []
+
+        for county in comparecounties:
+
+            sql_select_query  = """SELECT * FROM landkreis WHERE Stadtname = %s ORDER BY Zuletzt_geupdatet DESC"""  #SQL query
+            mycursor.execute(sql_select_query,(county,))    #takes input from DiscordBotDebug and puts in in %s above
+
+            myresult = mycursor.fetchall()
+
+            
+            date = []
+            
+
+            for x in myresult:      #search trough results of query
+            
+                date.append(str(x[6]))      
+                incidence.append(float(x[5]))
+        
+        county1inc = incidence[len(incidence)//2:]  #takes the first half of the values linked to the first county
+        county2inc = incidence[:len(incidence)//2] 
+        
+        mydb.close()
+        #df = pd.DataFrame({county: incidence},
+                            # index=date) 
+
+        df = pd.DataFrame({comparecounties[1]: county1inc,
+                            comparecounties[0]: county2inc}, index=date)            #create panda dataframe
+
+        
+        df = df[::-1]
+        ax = df.plot()
+
+        plt.xticks(rotation=30, horizontalalignment="center")
+        plt.title('Inzidenzwert von ' + county)  #name axis and title
+        plt.ylabel('Inzidenzfälle')
+        plt.xlabel('Datum')   
+
+        ax.set_ylim(bottom=0)
+        
+        figName = 'saved_figure.png'
+        fig = plt.gcf()
+        fig.set_size_inches((8.5, 11), forward=False)
+        fig.savefig(figName, dpi=500)
+        
+        imgdata = 'saved_figure.png'    #save and return the img path file
+        return imgdata
+
 
 def piechart(county):
     
